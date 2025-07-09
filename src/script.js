02efa3aa936d939bf7282585e416c2dfe9303a33
Vida <quiniavidamarie30@gmail.com>
2025-07-09 17:57:46 +0800

    started styling the website

--- src/script.js
+++ src/script.js
@@ -1,7 +1,38 @@
-async function getWeatherData(country){
-    const data = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${country}/2025-07-07?key=ZDLHZQNCD3ZBG72RLWFD5BLL2`, {mode: 'cors'})
+async function getWeatherData(country, firstDate, secondDate){
+    const data = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${country}/${firstDate}/${secondDate}?unitGroup=us&include=current%2Cdays%2Calerts&key=ZDLHZQNCD3ZBG72RLWFD5BLL2&contentType=json`, {mode: 'cors'})
     const weatherData = await data.json()
     console.log(weatherData)
+
+    const conditions = weatherData.currentConditions.conditions
+
+    const temperature = weatherData.currentConditions.temp
+    const tempFeelsLike = weatherData.currentConditions.feelslike
+    const uvIndex = weatherData.currentConditions.uvindex
+
+    const humidty = weatherData.currentConditions.humidity
+    const rainProbability = weatherData.currentConditions.precipprob
+
+    const windDirection = weatherData.currentConditions.winddir
+    const windSpeed = weatherData.currentConditions.windspeed
+
+    const timezone = weatherData.timezone
+    const time = weatherData.currentConditions.datetime
+
+    const sunriseTime = weatherData.currentConditions.sunrise
+    const sunsetTime = weatherData.currentConditions.sunset
+    
+    console.log(conditions)
+    console.log(temperature)
+    console.log(tempFeelsLike)
+    console.log(uvIndex)
+    console.log(humidty)
+    console.log(rainProbability)
+    console.log(windDirection)
+    console.log(windSpeed)
+    console.log(timezone)
+    console.log(time)
+    console.log(sunriseTime)
+    console.log(sunsetTime)
 }
 
-getWeatherData('pateros')
+getWeatherData('pateros', '2025-07-04', '2025-07-10')
